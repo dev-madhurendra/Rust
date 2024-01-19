f1473ded0e74e2da937cb1b6be74a79615c33b5a
madhuredra <madhuredra.tiwari@zemosolabs.com>
2024-01-19 16:53:15 +0530

    :sparkles: feat : hello word

---
+++ hello_word/main.rs
@@ -0,0 +1,8 @@
+// Entry point of the program 
+fn main() {
+    // Print "Hello, world!" followed by a newline
+    println!("Hello, world!");
+
+    // Print "Hello, World !" without a newline
+    print!("Hello, World !");
+}
